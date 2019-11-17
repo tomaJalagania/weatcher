bc54bd4b5ad4d00f7753ebb9c6ef1682b9300298
Toma jalagania <toma.jalagania@mail.ru>
2019-11-17 07:36:32 +0400

    build

---
+++ build/precache-manifest.aaad71ba672d5c328bf784483545be22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9260f364e1821482b61304f6da8c7730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b190ee437ce92619f5fd",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "f82da5c10713ffb58ccd",
+    "url": "/static/js/2.c2e87413.chunk.js"
+  },
+  {
+    "revision": "b190ee437ce92619f5fd",
+    "url": "/static/js/main.fc94eedf.chunk.js"
+  },
+  {
+    "revision": "e2f64e35e170fcb79dc5",
+    "url": "/static/js/runtime-main.a8fe9b23.js"
+  },
+  {
+    "revision": "19b90b13904d2529ee0c7abcbcc80a12",
+    "url": "/static/media/cloud.19b90b13.svg"
+  },
+  {
+    "revision": "e41c7d514dca1f3b5208fa666694641d",
+    "url": "/static/media/sun.e41c7d51.svg"
+  }
+]);
